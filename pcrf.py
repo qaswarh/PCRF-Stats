b07fe174879c285aadcd0071759001ed27d5cd2b
Qaswar Hussain <47313728+qaswarh@users.noreply.github.com>
2020-01-27 02:49:19 -0800

    Update pcrf.py

--- pcrf.py
+++ pcrf.py
@@ -5,7 +5,7 @@ with open('bulkpcrf.csv', 'r') as csv_file:     #replace with actual file name
     csv_reader = csv.reader(csv_file)
     table = list()
     for line in csv_reader:
-        st1 = '0' or '0.0'                      #if you want to ignore null records
+        st1 = '0' or '0.0'                      #if you want to discard null records
         st2 = 'AAR'                             #KPI, as per need change to other diameter cmds, cpu, memory etc.
         st3 = re.sub('.*f01-', '', line[1])     #re was imported to trim the name, otherwise st3 = line[1]
         st4 = line[2]
