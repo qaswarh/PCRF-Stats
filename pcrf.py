9fe711fa025a90555d917ee64dab87ba675218a5
qaswarh <qaswar@inabia.com>
2020-01-30 16:21:04 -0800

    Update pcrf.py

--- pcrf.py
+++ pcrf.py
@@ -1,13 +1,14 @@
 import csv
 import re
 import tabulate
-with open('bulkpcrf.csv', 'r') as csv_file:     #replace with actual file name
+
+with open('bulkpcrf.csv', 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
-    table = list()
+    table = []
     for line in csv_reader:
-        st1 = '0' or '0.0'                      #if you want to discard the null records
+        st1 = '0' or '0.0'                      #if you want to ignore null records
         st2 = 'AAR'                             #KPI, as per need change to other diameter cmds, cpu, memory etc.
-        st3 = re.sub('.*f01-', '', line[1])     #re was imported to trim the name, otherwise st3 = line[1]
+        st3 = re.sub('.*f01-', '', line[1])     #re was imported to hide some info, otherwise st3 = line[1]
         st4 = line[2]
         st5 = line[3]
         tr = [st3, st4, st5]
@@ -15,4 +16,9 @@ with open('bulkpcrf.csv', 'r') as csv_file:     #replace with actual file name
         if st2 in line[2]:
             if st1 not in line[3]:
                 table.append(tr)
+
     print(tabulate.tabulate(table, headers, tablefmt="fancy_grid"))
+    with open(st2 + '.csv', 'w', newline='') as csv_file:
+        a = csv.writer(csv_file)
+        a.writerows([headers])
+        a.writerows(table)
